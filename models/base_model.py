f9c50b3350cd21eeadc7d3f713075bf6efb2d180
Ikenna <iykethe1st@gmail.com>
2023-02-10 23:26:03 -0800

    Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,11 @@
 # Author: Chisom Ibekwe
 """This module contains the BaseModel class that defines all common
 attributes/methods for other classes."""
+
+import models
 import cmd
-import uuid
-import datetime
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel(cmd.Cmd):
@@ -16,22 +18,26 @@ class BaseModel(cmd.Cmd):
         Assigns a unique id to it, time created,
         and time updated (updates each time the object is changed).
         """
-        if kwargs:
-            out = ['__class__', 'created_at', 'updated_at']
-            self.__dict__.update((key, val) for key, val in kwargs.items() if key not in out)
-            self.created_at = datetime.datetime.today()
-            self.updated_at = self.created_at
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.today()
-            self.updated_at = self.created_at
+            models.storage.new(self)
 
     def __str__(self):
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """updates the instance attribute - updated_at with the current datetime."""
-        self.updated_at = datetime.datetime.today()
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """
